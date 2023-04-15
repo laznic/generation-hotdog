e1ad0618fe14a0f4c7f1ee3e2b337851cd15feb8
Niklas L <niklas@demtrees.com>
2023-04-16 00:28:26 +0300

    Add main navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { Outlet } from 'react-router'
+import { Outlet } from 'react-router-dom'
+import LinkWithAnimation from './modules/common/components/LinkWithAnimation';
+
 import { Toaster } from './modules/common/components/Toaster'
 
 function App() {
   return (
-    <div className="App">
+    <main className="App container mx-auto">
+      <nav className="border-t border-white my-8">
+        <ul className="flex items-center gap-16 font-bold text-3xl tracking-wide mt-8">
+          <li>
+            <LinkWithAnimation to={'/'}>
+              HOME
+            </LinkWithAnimation>
+          </li>
+          <li>
+          <LinkWithAnimation to={'/wall'}>
+              WALL
+          </LinkWithAnimation>
+          </li>
+        </ul>
+      </nav>
+
       <Outlet />
       <Toaster />
-    </div>
+    </main>
   )
 }
 

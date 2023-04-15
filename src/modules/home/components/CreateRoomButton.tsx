e6014754dde9f635deeaee3e188b7ecddb0c7372
Niklas L <niklas@demtrees.com>
2023-04-16 00:29:46 +0300

    Update button to match with designs

--- src/modules/home/components/CreateRoomButton.tsx
+++ src/modules/home/components/CreateRoomButton.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@/modules/common/components/Button"
 import supabase from '@/supabase'
-import { UpdateIcon } from "@radix-ui/react-icons"
+import { ArrowTopRightIcon, UpdateIcon } from "@radix-ui/react-icons"
+import { motion } from "framer-motion"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
@@ -36,9 +36,54 @@ export default function CreateRoomButton() {
   }
 
   return (
-    <Button onClick={createRoom} disabled={loading}>
-      {loading && <UpdateIcon className={'animate-spin mr-2'} />}
-      Create Room
-    </Button>
+    <motion.button
+      onClick={createRoom}
+      className="relative flex items-center w-fit"
+      disabled={loading}
+      whileHover="hover"
+      transition={{ duration: 0.2, ease: "easeInOut" }}>
+      <motion.span variants={buttonVariants} className="relative font-bold text-xl md:text-2xl 2xl:text-4xl pb-2">
+        BEGIN
+
+        <motion.div
+          variants={lineVariants}
+          className="absolute bottom-0 left-0 h-[2px] bg-white"
+          transition={{ duration: 0.1, ease: "easeInOut", delay: 0.1 }}
+        />
+      </motion.span>
+
+      <motion.div variants={arrowVariants} transition={{ duration: 0.2, ease: "easeInOut", delay: 0.1  }}>
+        {loading ? <UpdateIcon className="w-7 h-7 mb-1 animate-spin ml-2" /> : <ArrowTopRightIcon className="w-11 h-11 mb-1" />}
+      </motion.div>
+  </motion.button>
   )
+}
+
+const buttonVariants = {
+  default: {
+    y: 0,
+  },
+  hover: {
+    y: -4,
+  },
+};
+
+const lineVariants = {
+  default: {
+    width: "0%",
+  },
+  hover: {
+    width: "100%",
+  },
+};
+
+const arrowVariants = {
+  default: {
+    y: 0,
+    x: 0
+  },
+  hover: {
+    y: -6,
+    x: 4
+  }
 }

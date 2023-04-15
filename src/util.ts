87f15d5b59f200d4df19563790d03ecdae9ead0a
Niklas L <niklas@demtrees.com>
2023-04-15 19:23:23 +0300

    Add util to calculate reading time to animate stuff in and out nicely

--- src/util.ts
+++ src/util.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function calculateReadingTime(text: string, readingSpeed?: number) {
+  const wordsPerMinute = readingSpeed || 200; // Default reading speed is 200 words per minute
+  const words = text.split(' ').length;
+  const readingTimeInMinutes = words / wordsPerMinute;
+  const readingTimeInSeconds = readingTimeInMinutes * 60;
+  return Math.ceil(readingTimeInSeconds);
+}

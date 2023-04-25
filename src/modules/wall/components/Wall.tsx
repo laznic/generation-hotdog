04ce218eafb952b22c8873f464e5ab62b6eeef20
Niklas L <niklas@demtrees.com>
2023-04-25 11:13:52 +0300

    Remove list animation as it makes images load really slowly

--- src/modules/wall/components/Wall.tsx
+++ src/modules/wall/components/Wall.tsx
@@ -10,15 +10,11 @@ export default function Wall () {
 
   return (
     <div className="container mx-auto mt-[7vw] mb-8">
-      <motion.ul
-        initial="hidden"
-        variants={list}
-        animate="visible"
+      <ul
         className="grid gap-8 grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
         {data.map((hotdog) => (
           <motion.li
             className="rounded-md overflow-hidden"
-            variants={item}
             whileHover={{ scale: 1.05 }}
             key={hotdog.id}>
             <Link to={`/wall/${hotdog.id}`} className="bg-neutral-950 w-full">
@@ -26,28 +22,7 @@ export default function Wall () {
             </Link>
           </motion.li>
         ))}
-      </motion.ul>
+      </ul>
     </div>
   )
-}
-
-const list = {
-  visible: {
-    opacity: 1,
-    transition: {
-      when: "beforeChildren",
-      staggerChildren: 0.1,
-    },
-  },
-  hidden: {
-    opacity: 0,
-    transition: {
-      when: "afterChildren",
-    },
-  },
-}
-
-const item = {
-  visible: { opacity: 1, y: 0 },
-  hidden: { opacity: 0, y: -100 },
 }
